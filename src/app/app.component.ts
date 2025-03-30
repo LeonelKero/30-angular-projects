7093aae83abdba521f2789bba266f1e2d4acf35c
leonel ka <waboleonel@gmail.com>
2025-03-30 16:09:12 +0100

    Add and configure tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <h1 class="text-3xl text-center text-cyan-600 my-5 font-light">Welcome to {{title}}!</h1>
 
     <router-outlet />
   `,
