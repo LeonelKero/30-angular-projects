a790b94e0d617a9917d4dfb19957932fca392f8e
leonel ka <waboleonel@gmail.com>
2025-03-30 16:18:22 +0100

    Merge with remote main branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   template: `
-    <h1 class="text-3xl text-center text-cyan-600 my-5 font-light">Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <h1 class="text-3xl text-center text-cyan-600 my-5 font-light">Welcome to Angular v19 - 30 Projects</h1>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'angular-30-projects';
 }
